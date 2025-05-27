02746a27b740f23b8a5f265f86b5786a9b1e0d5a
metin7432 <metin7432@gmail.com>
2025-05-27 10:10:07 -0400

    editting Details page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -15,6 +16,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
