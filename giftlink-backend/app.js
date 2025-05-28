9a129e86c5e55c05447fbc81f8cd13c144e2ec9c
metin7432 <metin7432@gmail.com>
2025-05-28 13:25:49 -0400

    edittin authRoutes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-const giftRoutes = require('./routes/giftRoutes');
-const searchRoutes = require('./routes/searchRoutes');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -23,25 +21,18 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-app.use('./api/gifts',giftRoutes)
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('./api/search', searchRoutes)
-
-
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
